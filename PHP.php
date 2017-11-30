574981c3e6e3fd255be6d7ca854b9367b72c2195
make674927964 <674927964@qq.com>
2017-12-01 00:50:37 +0800

    'ajax'

---
+++ PHP.php
@@ -0,0 +1,16 @@
+<?php
+$user = $_GET['user'];
+$pass = $_GET['pass'];
+if ($user == "admin") {
+    if ($pass == "123456") {
+        echo "登入成功";
+    }
+    else {
+        echo "登入错误";
+    }
+}
+else {
+    echo "账号不存在";
+}
+
+
